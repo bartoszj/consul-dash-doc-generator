f1eb71390754e6fb8d3eeaecc208be00c3531f89
Bartosz Janda <bjanda@pgs-soft.com>
2019-01-01 19:14:16 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+Consul Dash Docs Generator
+==========================
+
+This projects essence has been taken from [https://github.com/rolandjohann/terraform-dash-doc-generator](https://github.com/rolandjohann/terraform-dash-doc-generator) and updated to generate Packer docs.
+
+### Installation
+
+```bash
+rbenv install 2.6.0
+gem install -N bundler
+bundle install
+```
+
+### Build
+
+To build execute command:
+
+```bash
+./build.sh <version>
+```
+
+Then move the docset into a proper directory.
---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Read parameters
+TAG=$1
+if [ -z $TAG ]; then
+    echo '"TAG" must be specified'
+    exit 1
+fi
+
+# Paths
+CWD=$(pwd)
+BUILD_PATH="${CWD}/build/$TAG"
+CONSUL_PATH="${CWD}/consul"
+WEBSITE_PATH="${CONSUL_PATH}/website"
+
+# Clean build
+rm -rf "${BUILD_PATH}"
+mkdir -p "${BUILD_PATH}"
+
+# Checkout and clean
+git clone "https://github.com/hashicorp/consul.git" || true
+cd "${CONSUL_PATH}"
+git fetch --all --prune
+git checkout -- .
+git checkout "v${TAG}"
+
+# # Install gems
+# cd "${WEBSITE_PATH}"
+# bundle install
+
+# rm Rakefile || true
+# # cp "${CWD}/Rakefile" .
+# ln -s "${CWD}/Rakefile" || true
+
+# # Build
+# rake
+
+# mv Consul.tgz "${BUILD_PATH}"
